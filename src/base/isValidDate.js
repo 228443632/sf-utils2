280977165651962e09c3f61d08c2b4b37d27f4b9
bianpengfei <228443632@qq.com>
2024-01-25 12:28:42 +0800

    fix: 修复判断日期错误问题

--- src/base/isValidDate.js
+++ src/base/isValidDate.js
@@ -1,12 +1,15 @@
 import isDate from '@/base/isDate'
+import isNullable from '@/base/isNullable'
 
 /**
  * 判断是否是一个合法的date
  * @param {any} val
  * @returns {boolean}
+ *
  */
 function isValidDate(val) {
-  return isDate(val) && !isNaN(val)
+  if (isNullable(val)) return false
+  return isDate(val)
 }
 
 export default isValidDate
