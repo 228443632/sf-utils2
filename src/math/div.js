48241c83abaa897f7561566c0a3d87aaebd8feef
bianpengfei <228443632@qq.com>
2024-04-15 13:16:29 +0800

    fix: 修复div分母当是0活着undefined报错问题

--- src/math/div.js
+++ src/math/div.js
@@ -15,7 +15,8 @@ function div(...args) {
   return args
     .slice(1)
     .reduce((pre, cur) => {
-      if (!getNumString(cur) || cur == '0') return new Decimal(0) // 如果分母是 0，则返回0
+      const numString = getNumString(cur)
+      if (numString == '0' || !numString || cur == '0') return new Decimal(0) // 如果分母是 0，则返回0
       return pre.div(cur)
     }, new Decimal(getNumString(args[0])))
     .toFixed()
